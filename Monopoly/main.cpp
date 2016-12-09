18244f68031e3c87ec6db36babec9f20af71f387
CMilby <cmilby99@gmail.com>
2016-12-09 13:45:07 -0500

    Initial Commit

---
+++ Monopoly/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Monopoly
+//
+//  Created by Craig Milby on 12/9/16.
+//  Copyright © 2016 Craig Milby. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
